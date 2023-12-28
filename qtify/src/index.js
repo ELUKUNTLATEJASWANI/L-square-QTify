ef564827c0e22c935e8f42d29d5913643390b57c
ELUKUNTLATEJASWANI <tejaswanielukuntla@gmail.com>
2023-12-28 16:43:58 +0530

    Albums Sections

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -1,24 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+// import HomePage from './pages/HomePage/HomePage';
+
+// const router=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<App/>,
+//     children:[
+//       {
+//         path:"/",
+//         element:<HomePage />
+//       }
+//     ]
+//   }
+// ])
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<RouterProvider router={router} />);
+
+// reportWebVitals();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
 
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>,
-    children:[
-      {
-        path:"/",
-        element:<HomePage />
-      }
-    ]
-  }
-])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
-
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
